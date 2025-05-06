bdececaa0e606b67e82f0b0ea367431d3806e282
Vina del Mar <vina@duoc.acad>
2025-05-06 13:39:05 -0400

    agregamos requestMapping

--- MICROSERVICIO_DE_VENTAS-20250502T134205Z-001/MICROSERVICIO_DE_VENTAS/ventas/src/main/java/com/microservicio/ventas/Controller/ventaController.java
+++ MICROSERVICIO_DE_VENTAS-20250502T134205Z-001/MICROSERVICIO_DE_VENTAS/ventas/src/main/java/com/microservicio/ventas/Controller/ventaController.java
@@ -8,26 +8,28 @@ import com.microservicio.ventas.model.Venta;
 import com.microservicio.ventas.service.VentaService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
 @RestController
+@RequestMapping("/api/v1/ventas")
 public class ventaController {
     @Autowired
     private VentaService ventaService;
 
 
-    @PostMapping("/hacerVenta")
+    @PostMapping("/")
     ResponseEntity<String> obtenerVenta(@RequestBody Venta venta){
         return ResponseEntity.ok(ventaService.crearVenta(venta));
     }
     
 
     
-    @GetMapping("/obtenerVenta/{idVenta}")
+    @GetMapping("/{idVenta}")
     public ResponseEntity<Venta> obtenerVenta(@PathVariable int idVenta){ {
         return ResponseEntity.ok(ventaService.traerVenta(idVenta));
     }
